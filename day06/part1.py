0eff70623dc19d432dcb15fbc699ae28e66965ee
duncsully <duncsully@gmail.com>
2022-12-08 09:49:15 -0600

    day six
    Python, you're beautiful

---
+++ day06/part1.py
@@ -0,0 +1,19 @@
+import sys
+from collections import deque
+from itertools import repeat
+
+fileLocation = None
+if (len(sys.argv) >= 2):
+    fileLocation = sys.argv[1]
+else:
+    fileLocation = input(
+        "Enter input file location: ")
+
+with open(fileLocation) as file:
+    lastFour = deque([], 4)
+    line = file.readline().strip()
+    for i, char in enumerate(line):
+        lastFour.append(char)
+        if (len(set(lastFour)) == 4):
+            break
+print(f'It took {i + 1} characters to start-of-packet marker')
---
+++ day06/part2.py
@@ -0,0 +1,19 @@
+import sys
+from collections import deque
+from itertools import repeat
+
+fileLocation = None
+if (len(sys.argv) >= 2):
+    fileLocation = sys.argv[1]
+else:
+    fileLocation = input(
+        "Enter input file location: ")
+
+with open(fileLocation) as file:
+    lastFourteen = deque([], 14)
+    line = file.readline().strip()
+    for i, char in enumerate(line):
+        lastFourteen.append(char)
+        if (len(set(lastFourteen)) == 14):
+            break
+print(f'It took {i + 1} characters to start-of-packet marker')
